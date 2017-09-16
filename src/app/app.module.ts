26576d40ab59ffb9eedad6b8e0caa18d884c3c33
Leandro Castro Santana <leandrocastro.ciencia@gmail.com>
2017-09-16 11:45:35 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+
+
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { FeedPage } from '../pages/feed/feed';
+import { IntroPage } from '../pages/intro/intro';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+  
+    HomePage,
+    TabsPage,
+    FeedPage,
+    IntroPage,
+   
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  
+    
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+  
+    HomePage,
+    TabsPage,
+    FeedPage,
+    IntroPage,
+    
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/intro/intro.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { IntroPage } from './intro';
+
+@NgModule({
+  declarations: [
+    IntroPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(IntroPage),
+  ],
+
+
+  exports:[
+    
+    IntroPage
+    
+    ]
+
+
+})
+export class IntroPageModule {}
